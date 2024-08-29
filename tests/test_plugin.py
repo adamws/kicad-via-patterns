f17ba013b06d5bf53a7e939c1c6d92770d88a91b
adamws <adamws@users.noreply.github.com>
2024-08-29 18:15:50 +0200

    Add tests for plugin's log setup and KiCad version check

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,19 +1,34 @@
 import logging
 import os
-import sys
 from pathlib import Path
+from unittest.mock import MagicMock, patch
 
 import pcbnew
+import pytest
+
+from via_patterns.plugin_action import get_kicad_version, setup_logging
 
 logger = logging.getLogger(__name__)
 
 
 def test_if_plugin_loads() -> None:
-    version = pcbnew.Version()
-    logger.info(f"Plugin executed with KiCad version: {version}")
-    logger.info(f"Plugin executed with python version: {sys.version!r}")
-
     dirname = Path(os.path.realpath(__file__)).parents[1]
     pcbnew.LoadPluginModule(dirname, "via_patterns", "")
     not_loaded = pcbnew.GetUnLoadableWizards()
     assert not_loaded == "", pcbnew.GetWizardsBackTrace()
+
+
+def test_setup_logging(tmpdir: Path) -> None:
+    setup_logging(str(tmpdir))
+    assert Path(f"{tmpdir}/plugin.log").exists()
+
+
+def test_get_kicad_version() -> None:
+    assert get_kicad_version()[0] in ["7", "8"]
+
+
+@patch("pcbnew.Version")
+def test_get_kicad_version_unsupported(mock_pcbnew_version: MagicMock) -> None:
+    mock_pcbnew_version.return_value = "6.0.11"
+    with pytest.raises(Exception, match="KiCad version 6.0.11 is not supported"):
+        get_kicad_version()
